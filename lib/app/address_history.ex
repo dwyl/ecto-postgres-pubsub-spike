752426b18c75d7a635cf99c8126f94c6962dfbb0
Robert Francis <robertefrancis18@gmail.com>
2019-04-29 15:45:14 +0100

    adds ref_id to address history #1

--- lib/app/address_history.ex
+++ lib/app/address_history.ex
@@ -1,8 +1,9 @@
-defmodule App.Address_history do
+defmodule App.AddressHistory do
   use Ecto.Schema
   import Ecto.Changeset
 
   schema "addresses_history" do
+    field :ref_id, :integer
     field :address_line_1, :string
     field :address_line_2, :string
     field :city, :string
@@ -16,7 +17,7 @@ defmodule App.Address_history do
   @doc false
   def changeset(address_history, attrs) do
     address_history
-    |> cast(attrs, [:name, :address_line_1, :address_line_2, :city, :postcode, :tel])
-    |> validate_required([:name, :address_line_1, :address_line_2, :city, :postcode, :tel])
+    |> cast(attrs, [:ref_id, :name, :address_line_1, :address_line_2, :city, :postcode, :tel])
+    |> validate_required([:ref_id, :name, :address_line_1, :address_line_2, :city, :postcode, :tel])
   end
 end
