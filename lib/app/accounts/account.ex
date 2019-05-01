64b4f5537f10fb69832f0355de469bdcd958157f
Robert Francis <robertefrancis18@gmail.com>
2019-05-01 17:05:51 +0100

    log data from trigger #1

--- lib/app/accounts/account.ex
+++ lib/app/accounts/account.ex
@@ -1,8 +1,6 @@
 defmodule App.Accounts.Account do
   use Ecto.Schema
 
-  # @primary_key {:id, :binary_id, autogenerate: true}
-
   schema "accounts" do
     field(:username, :string)
   end
--- lib/app/listener.ex
+++ lib/app/listener.ex
@@ -10,41 +10,27 @@ defmodule App.Listener do
   end
 
   def start_link(opts \\ []) do
-    IO.inspect("App Listener start_link")
     GenServer.start_link(__MODULE__, opts)
   end
 
   @impl true
   def init(opts) do
-    IO.inspect("App Listener init")
     with {:ok, _pid, _ref} <- App.Repo.listen("accounts_changed") do
-      IO.inspect("App Listener init worked")
       {:ok, opts}
     else
       error ->
-        IO.inspect("App Listener init error")
         {:stop, error}
     end
   end
 
   @impl true
   def handle_info({:notification, _pid, _ref, "accounts_changed", payload}, _state) do
-    IO.inspect("App Listener handle_info")
     with {:ok, data} <- Jason.decode(payload, keys: :atoms) do
-      data
-      |> inspect()
-      |> Logger.info()
-
+      IO.inspect(data, label: "====> data in handle info")
       {:noreply, :event_handled}
     else
-      error -> {:stop, error, []}
+      error ->
+        {:stop, error, []}
     end
-  end
-
-  def handle_info(arg1, state) do
-    IO.inspect("====================================")
-    IO.inspect(arg1)
-    IO.inspect(state)
-    IO.inspect("====================================")
   end
 end
