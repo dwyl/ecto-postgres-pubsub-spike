6ee1f97027cdd19dc3dbe39397138a586a6c9355
Robert Francis <robertefrancis18@gmail.com>
2019-05-01 17:05:12 +0100

    creates postgres function and a procedure that calls said function on insert or updated to accounts table #1

--- priv/repo/migrations/20190429160525_create_accounts.exs
+++ priv/repo/migrations/20190429160525_create_accounts.exs
@@ -1,9 +1,40 @@
 defmodule App.Repo.Migrations.CreateAccounts do
   use Ecto.Migration
 
-  def change do
+  def up do
+    execute """
+    CREATE OR REPLACE FUNCTION notify_account_changes()
+    RETURNS trigger AS $$
+    BEGIN
+      PERFORM pg_notify(
+        'accounts_changed',
+        json_build_object(
+          'operation', TG_OP,
+          'record', row_to_json(NEW)
+        )::text
+      );
+
+      RETURN NEW;
+    END;
+    $$ LANGUAGE plpgsql;
+    """
+
     create table(:accounts) do
       add(:username, :string, null: true)
     end
+
+    flush()
+
+    execute """
+    CREATE TRIGGER accounts_changed
+    AFTER INSERT OR UPDATE
+    ON accounts
+    FOR EACH ROW
+    EXECUTE PROCEDURE notify_account_changes()
+    """
+  end
+
+  def down do
+    drop table(:accounts)
   end
 end
